48cc527f30bfb55104e27df1f2a0fe453e882918
Binuri <pererabinuri29@gmail.com>
2025-05-01 09:02:01 +0530

    replace require with import in tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,28 +9,20 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
-    themes:[
+    themes: [
       { mytheme: {
-          
-      "primary": "#dc2626",
-                
-      "secondary": "#dc2626",
-                
-      "accent": "#dc2626",
-                
-      "neutral": "#ff00ff",
-                
-      "base-100": "#ffffff",
-                
-      "info": "#2563eb",
-                
-      "success": "#22c55e",
-                
-      "warning": "#f87171",
-                
-      "error": "#ff0000",
-                },},],
+        "primary": "#dc2626",
+        "secondary": "#dc2626",
+        "accent": "#dc2626",
+        "neutral": "#ff00ff",
+        "base-100": "#ffffff",
+        "info": "#2563eb",
+        "success": "#22c55e",
+        "warning": "#f87171",
+        "error": "#ff0000",
+      }},
+    ],
   }
 }
